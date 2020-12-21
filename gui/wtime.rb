6b62d7314ac30fd3bbd8d04070d5f3467b721b01
cnngimenez <christian.gimenez@fi.uncoma.edu.ar>
2020-12-21 14:53:16 -0300

    Time widget.

---
+++ gui/wtime.rb
@@ -0,0 +1,76 @@
+# Copyright 2020 Christian Gimenez
+# 
+# wtime.rb
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'fox16'
+
+include Fox
+
+module GUI
+  class WTime < FXHorizontalFrame
+    def initialize(...)
+      super(...)
+
+      @time = Time.now
+
+
+      
+      @fxhour = FXTextField.new self, 2, :opts => TEXTFIELD_INTEGER | TEXTFIELD_LIMITED
+      @fxhour.connect SEL_VERIFY do |sender, sel, data|
+        if data.to_i > 24
+          @fxstart.text = "1"
+        elsif data.to_i <= 0
+          @fxstart.text = "1"
+        else
+          @fxstart.text = data
+        end
+      end
+      @fxhour.connect SEL_CHANGED do |sender, sel, data|
+        update_time
+      end
+
+      
+      @fxsep = FXLabel.new self, ' : '
+      
+      @fxmin = FXTextField.new self, 2, :opts => TEXTFIELD_INTEGER | TEXTFIELD_LIMITED
+      @fxmin.connect SEL_VERIFY do |sender, sel, data|
+        if data.to_i >= 60
+          @fxstart.text = "0"
+        elsif data.to_i < 0
+          @fxstart.text = "0"
+        else
+          @fxstart.text = data
+        end
+      end
+      @fxmin.connect SEL_CHANGED do |sender, sel, data|
+        update_time
+      end
+      
+      update_widgets
+    end
+
+    private 
+    def update_widgets
+      @fxhour.text = @time.hour.to_s
+      @fxmin.text = @time.min.to_s
+    end
+
+    def update_time
+      @time = Time.new nil, nil, nil, @fxhour.text.to_i, @fxmin.text.to_i, nil, nil
+    end
+    
+  end # WTime
+end # GUI
