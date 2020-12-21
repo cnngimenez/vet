d2d47f432ec28d11ab8395d9aed2100d9c94fc85
cnngimenez <christian.gimenez@fi.uncoma.edu.ar>
2020-12-21 14:53:22 -0300

    Using time widget.

--- gui/wappointment.rb
+++ gui/wappointment.rb
@@ -1,15 +1,32 @@
+# coding: utf-8
 require 'fox16'
-
 include Fox
+
+require_relative 'wtime'
 
 module GUI
   # Appointment widget
-  class WAppointment < FXGroupBox
-    def initialize(parent)
-      super(parent, "Appointments", :opts => LAYOUT_FILL_X)
+  class WAppointment < FXVerticalFrame
+    def initialize(...)
+      super(...)
       
       @fxlist = FXList.new self, :opts => LAYOUT_FILL_X
-      @fxbtn = FXButton.new self, "Ver", :opts => LAYOUT_FILL_X
+      
+      @fxtxt = FXTextField.new self, 50, :opts => LAYOUT_FILL_X
+      @fxtxt.text = "Título"
+      @fxtxt.helpText = "Título del turno"
+      @fxtxt.tipText= "Título del turno"
+
+      @wstart = WTime.new self
+      @wend = WTime.new self
+
+      @fxfdesc = FXText.new self, :opts => LAYOUT_FILL_X
+      
+      @fxbtn = FXButton.new self, "Nuevo Turno", :opts => LAYOUT_FILL_X
+      @fxbtn.connect SEL_COMMAND do |sender, sel, data|
+        @fxlist.appendItem @fxtxt.text
+        @fxtxt.text = ""
+      end
     end
   end # WAppointment
 end
