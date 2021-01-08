4752f0472de8da042d5c64de35039aed9b0e19c6
cnngimenez <christian.gimenez@fi.uncoma.edu.ar>
2021-01-08 14:00:03 -0300

    Adding txtcost to GUI.

--- gui/inventory/wproduct.rb
+++ gui/inventory/wproduct.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 # Copyright 2020 Christian Gimenez
-# 
+#
 # wproduct.rb
 #
 # This program is free software: you can redistribute it and/or modify
@@ -16,42 +16,48 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# frozen_string_literal: true
+
 require 'fox16'
-include Fox
-
 require_relative '../../models'
-include Models
 
+# User interface module.
 module GUI
+  include Fox
+  include Models
+  # Product widget
+  #
+  # Display and edit a product instance.
   class WProduct < FXGroupBox
     def initialize(...)
       super(...)
 
       @product = nil
-      
-      @lblname = FXLabel.new self, "Nombre:"
-      @txtname = FXTextField.new self, 50, :opts => LAYOUT_FILL_X | TEXTFIELD_NORMAL 
-      @lblcode = FXLabel.new self, "Código:"
-      @txtcode = FXTextField.new self, 4, :opts => LAYOUT_FILL_X | TEXTFIELD_NORMAL
-      @lblstock = FXLabel.new self, "Stock inicial:"
-      @txtstock = FXTextField.new self, 4, :opts => LAYOUT_FILL_X | TEXTFIELD_NORMAL |
-                                                   TEXTFIELD_INTEGER
+
+      @lblname = FXLabel.new self, 'Nombre:'
+      @txtname = FXTextField.new self, 50, opts: LAYOUT_FILL_X | TEXTFIELD_NORMAL
+      @lblcode = FXLabel.new self, 'Código:'
+      @txtcode = FXTextField.new self, 4, opts: LAYOUT_FILL_X | TEXTFIELD_NORMAL
+      @lblstock = FXLabel.new self, 'Stock inicial:'
+      @txtstock = FXTextField.new self, 4, opts: LAYOUT_FILL_X | TEXTFIELD_NORMAL | TEXTFIELD_INTEGER
+      @lblcost = FXLabel.new self, 'Precio unitario de venta:'
+      @txtcost = FXTextField.new self, 4, opts: LAYOUT_FILL_X | TEXTFIELD_NORMAL | TEXTFIELD_REAL
 
       update_widgets
     end
 
     def product
       if @product.nil?
-        return new_product
+        new_product
       else
         update_data
-        return @product
+        @product
       end
     end
 
     def edit(product)
       @product = product
-      update_widgets        
+      update_widgets
     end
 
     def reset
@@ -62,15 +68,17 @@ module GUI
     def new_product
       Product.create name: @txtname.text,
                      code: @txtcode.text,
-                     stock: @txtstock.text.to_i
+                     stock: @txtstock.text.to_i,
+                     unitary_cost: @txtcost.text.to_f
     end
-    
+
     private
+
     def update_widgets
       if @product.nil?
-        @txtname.text = ""
-        @txtcode.text = ""
-        @txtstock.text = "0"
+        @txtname.text = ''
+        @txtcode.text = ''
+        @txtstock.text = '0'
       else
         @txtname.text = @product.name
         @txtcode.text = @product.code
@@ -80,10 +88,10 @@ module GUI
 
     def update_data
       return if @product.nil?
+
       @product.name = @txtname.text
       @product.code = @txtcode.text
       @product.stock = @txtstock.text.to_i
     end
-    
-  end # WProduct
-end # GUI
+  end
+end
