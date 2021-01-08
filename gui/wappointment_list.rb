eaa1952b81489dc4a89d493d970cc67e92d0c170
cnngimenez <christian.gimenez@fi.uncoma.edu.ar>
2021-01-08 17:35:07 -0300

    Fixed layout on WAppointment_List

--- gui/wappointment_list.rb
+++ gui/wappointment_list.rb
@@ -1,6 +1,5 @@
-# coding: utf-8
 # Copyright 2020 Christian Gimenez
-# 
+#
 # wappointment_list.rb
 #
 # This program is free software: you can redistribute it and/or modify
@@ -16,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 require 'fox16'
 include Fox
 
@@ -28,60 +26,31 @@ module GUI
   # Appointment widget
   class WAppointment_List < FXMDIChild
     def initialize(mdiclient)
-      super mdiclient, 'Turnos', nil, nil, 0, 10, 10, 700, 700
+      super mdiclient, 'Turnos', nil, nil, 0, 10, 10, 700, 500
 
       @lst = []
-      
-      @fxh1 = FXHorizontalFrame.new self,
-                                    :opts => LAYOUT_FILL_X | LAYOUT_FILL_Y
-      @cal = FXCalendar.new @fxh1
-      @cal.connect SEL_COMMAND do |sender, sel, data|
-        set_date data
-      end
-      
-      @fxv1 = FXVerticalFrame.new @fxh1, :opts => LAYOUT_FILL_X | LAYOUT_FILL_Y
 
-      @f2 = FXHorizontalFrame.new @fxv1, :opts => LAYOUT_FILL_X
-      @btnyesterday = FXButton.new @f2, "Ayer"
-      @btnyesterday.connect SEL_COMMAND do |sender, sel, data|
-        set_date Time.now - 86400 # = 1 day
-      end
-      @btntoday = FXButton.new @f2, "Hoy"
-      @btntoday.connect SEL_COMMAND do |sender, sel, data|
-        set_date Time.now
-      end
-      @btntomorrow = FXButton.new @f2, "Mañana"
-      @btntomorrow.connect SEL_COMMAND do |sender, sel, data|
-        set_date Time.now + 86400 # = 1 day
-      end
-      
-      @fxdate = FXLabel.new @fxv1, "Turnos para hoy"
-      @fxlist = FXList.new @fxv1, :opts => LAYOUT_FILL_X | LAYOUT_FILL_Y | LIST_NORMAL
-      @fxlist.connect SEL_DOUBLECLICKED do |sender, sel, data|
-        edit_appointment_num data
-      end
+      fx1 = FXVerticalFrame.new self, opts: LAYOUT_FILL_X | LAYOUT_FILL_Y
 
-      @wap = WAppointment.new self, "Nuevo Turno",
-                              :opts => LAYOUT_FILL_X | LAYOUT_FILL_Y | FRAME_NORMAL
-      
-      @fxbtn = FXButton.new self, "Nuevo Turno",
-                            :opts => LAYOUT_CENTER_X | BUTTON_NORMAL 
-      @fxbtn.connect SEL_COMMAND do |sender, sel, data|
-        ap = @wap.appointment
-        ap.save
-        
-        add_appointment ap unless @lst.member? ap
-                
-        reset_input
-        update_widgets
-      end
+      # Calendar frame
+      fxh1 = FXHorizontalFrame.new fx1, opts: LAYOUT_FILL_X | LAYOUT_FILL_Y
+      @fcal = FXCalendar.new fxh1
 
-      # Avoid deleting when closing
-      self.connect SEL_CLOSE do
-        hide
-        1
-      end
-      
+      # List frame
+      fxv1 = FXVerticalFrame.new fxh1, opts: LAYOUT_FILL_X | LAYOUT_FILL_Y
+      # +- Buttons on the top of the list
+      f2 = FXHorizontalFrame.new fxv1, opts: LAYOUT_FILL_X
+      @btnyesterday = FXButton.new f2, 'Ayer'
+      @btntoday = FXButton.new f2, 'Hoy'
+      @btntomorrow = FXButton.new f2, 'Mañana'
+      # +- List
+      @fxdate = FXLabel.new fxv1, 'Turnos para hoy'
+      @fxlist = FXList.new fxv1, opts: LAYOUT_FILL_X | LAYOUT_FILL_Y | LIST_NORMAL
+
+      @wap = WAppointment.new fx1, 'Nuevo Turno', opts: LAYOUT_FILL_X | LAYOUT_FILL_Y | FRAME_NORMAL
+      @fxbtn = FXButton.new fx1, 'Nuevo Turno', opts: LAYOUT_CENTER_X | BUTTON_NORMAL
+
+      assign_handlers
       set_date Time.now # Also calls update_widgets
     end
 
@@ -93,18 +62,18 @@ module GUI
       return if appointment.nil?
 
       @wap.edit appointment
-      @fxbtn.text = "Guardar Turno"
+      @fxbtn.text = 'Guardar Turno'
     end
-    
+
     def set_date(date)
-      @fxdate.text = "Turnos para " + date.strftime("%D")
+      @fxdate.text = 'Turnos para ' + date.strftime('%D')
       @lst = Appointment.filter_by_date date
       update_widgets
     end
-    
+
     def reset_input
       @wap.reset
-      @fxbtn.text = "Nuevo Turno"
+      @fxbtn.text = 'Nuevo Turno'
     end
 
     def lst=(lst_appointments)
@@ -112,22 +81,53 @@ module GUI
       update_widgets
     end
 
-    def lst
-      return @lst
-    end
-    
+    attr_reader :lst
+
     def add_appointment(appointment)
       @lst.push appointment
       update_widgets
     end
 
     private
-    
+
+    def assign_handlers
+      @fcal.connect SEL_COMMAND do |_sender, _sel, data|
+        set_date data
+      end
+      @btnyesterday.connect SEL_COMMAND do |_sender, _sel, _data|
+        set_date Time.now - 86_400 # = 1 day
+      end
+      @btntoday.connect SEL_COMMAND do |_sender, _sel, _data|
+        set_date Time.now
+      end
+      @btntomorrow.connect SEL_COMMAND do |_sender, _sel, _data|
+        set_date Time.now + 86_400 # = 1 day
+      end
+      @fxlist.connect SEL_DOUBLECLICKED do |_sender, _sel, data|
+        edit_appointment_num data
+      end
+      @fxbtn.connect SEL_COMMAND do |_sender, _sel, _data|
+        ap = @wap.appointment
+        ap.save
+
+        add_appointment ap unless @lst.member? ap
+
+        reset_input
+        update_widgets
+      end
+
+      # Avoid deleting when closing
+      connect SEL_CLOSE do
+        hide
+        1
+      end
+    end
+
     def update_widgets
       @fxlist.clearItems
       @lst.each do |ap|
         @fxlist.appendItem ap.to_s
-      end      
-    end   
-  end # WAppointment_List
+      end
+    end
+  end
 end
