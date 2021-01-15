f5bd439dcfca47cd47c285f9c088899cbfefc257
cnngimenez <christian.gimenez@fi.uncoma.edu.ar>
2021-01-15 15:37:04 -0300

    Sell can be edited at WSellList

--- gui/inventory/wsellslist.rb
+++ gui/inventory/wsellslist.rb
@@ -19,15 +19,42 @@
 
 require_relative '../../models'
 require_relative 'winventorylist'
+require_relative 'wsell_edit'
 
 module GUI
   module Inventory
     # List all Sell instances (sold items) filtered by a particular date.
     class WSellsList < WInventoryList
+      def initialize(...)
+        super(...)
+
+        @wselledit = WSellEdit.new @flistframe, 'Editar Venta', opts: FRAME_NORMAL | LAYOUT_FILL_Y
+
+        assign_handlers
+      end
+
+      
       protected
       
       def from_date(date)
         Models::Sell.between_dates  from: date, to: date + 1.day
+      end
+
+      def on_flist_doubleclicked(_sender, _sel, data)
+        obj = @lstobjs[data]
+        @wselledit.purchase = obj
+      end
+
+      def on_wselledit_save
+        @wselledit.enable false
+        update_widgets
+      end
+      
+      def assign_handlers
+        super
+
+        @wselledit.on :on_save, method(:on_wselledit_save)
+        @flist.connect SEL_DOUBLECLICKED, method(:on_flist_doubleclicked)        
       end
     end
   end
